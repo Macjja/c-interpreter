b8dfd4e4218b1024a6adb0ae27e74d44fb39c96e
Macjja <84208129+Macjja@users.noreply.github.com>
2025-06-07 14:22:41 -0400

    Add 6-Functions to parsing
    Now added parsing of some functions, and of course some variables

--- main.c
+++ main.c
@@ -22,6 +22,9 @@ int *pc, *bp, *sp, ax, cycle; // virual machine registers
 // bp is a pointer to some element of the stack
 // sp is the pointer to the top of the stack
 // ax is the general purpose register
+//
+int index_of_bp; // index of bp pointer on stack
+
 
 // instructions
 enum {LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
@@ -302,7 +305,8 @@ void expression(int level) {
 
 void enum_declaration() {
   // parse enum [id] { a = 1, b = 3, ...}
-  int i = 0;
+  int i;
+  i = 0;
   while (token != '}') {
     if (token != Id) {
       printf("%d: ba enum identifier %d\n", line, token);
@@ -327,6 +331,137 @@ void enum_declaration() {
     if (token == ',') {
       next();
     }
+  }
+}
+
+void function_body() {
+  // type func_name (...) {...}
+  //                   -->|   |<--
+
+  // ... {
+  // 1. local declarations
+  // 2. statements
+  // }
+
+  int pos_local; // position of local variables on the stack.
+  int type; 
+  pos_local = index_of_bp;
+
+  while (token == Int | token == Char) {
+    // local variable declaration, just like global ones.
+    basetype = (token == Int) ? INT : CHAR;
+    match(token);
+
+    while (token != ';') {
+      type = basetype;
+      while (token == Mul) {
+        match(Mul);
+        type = type + PTR;
+      }
+
+      if (token != Id) {
+        // invalid declaration
+        printf("%d: bad local declaration\n", line);
+        exit(-1);
+      }
+      if (current_id[Class] == Loc) {
+        // identifier exists
+        printf("%d: duplicate local declaration\n", line);
+        exit(-1);
+      }
+      match(Id);
+      
+      // store the local variable
+      current_id[BClass] = current_id[Class]; current_id[Class] = Loc;
+      current_id[BType] = current_id[Type]; current_id[Type] = type;
+      current_id[BValue] = current_id[Value]; current_id[Value] = ++pos_local;
+
+      if (token == ',') {
+        match(',');
+      }
+    }
+
+    match(';');
+  }
+
+  // save the stack size for local variables;
+  *++text = ENT;
+  *++text = pos_local - index_of_bp;
+
+  // statements
+  while (token != '}') {
+    statement();
+  }
+
+  // emit code for leaving the sub function
+  *++text = LEV;
+}
+
+
+void function_parameter() {
+  int type;
+  int params;
+  params = 0;
+  while (token != ')') {
+    // type {'*'} id {',' type {'*'} id}
+
+    type = INT;
+    if (token == Int) {
+      match(Int);
+    } else if (token == Char) {
+      type = CHAR;
+      match(Char);
+    }
+
+    // pointer type
+    while (token == Mul) {
+      match(Mul);
+      type = type + PTR;
+    }
+
+    // parameter name
+    if (token != Id) {
+      printf("%d: bad parameter declaration\n", line);
+      exit(-1);
+    }
+    if (current_id[Class] == Loc) {
+      printf("%d: duplicate parameter declaration\n", line);
+      exit(-1);
+    }
+
+    match(Id);
+
+    // backup global var, then set info for local var of same name
+    current_id[BClass] = current_id[Class]; current_id[Class] = Loc;
+    current_id[BType] = current_id[Type]; current_id[Type] = type;
+    current_id[BValue] = current_id[Value]; current_id[Value] = params++; // index of current param
+
+    if (token == ',') {
+      match(',');
+    }
+  }
+  index_of_bp = params + 1;
+}
+
+void function_declaration() {
+  // type func_name (...) {...}
+  //                | this part
+  match('(');
+  function_parameter();
+  match(')');
+  match('{');
+  function_body();
+  // match('}'); leave match to be consumed by global_declaration
+
+  // unwind local variable declarations for all local variables.
+  current_id = symbols;
+  while (current_id[Token]) {
+    if (current_id[Class] == Loc) {
+      current_id[Class] = current_id[BClass];
+      current_id[Type] = current_id[BType];
+      current_id[Value] = current_id[BValue];
+    }
+    current_id = current_id + IdSize;
   }
 }
 
